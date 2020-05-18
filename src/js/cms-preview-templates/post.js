f041b8b6354412695c13c0d54f618a76436c1755
SGurupad <gurupad@appinessworld.com>
2020-05-18 13:59:22 +0530

    index changes

--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -3,20 +3,71 @@ import format from "date-fns/format";
 
 export default class PostPreview extends React.Component {
   render() {
-    const {entry, widgetFor, getAsset} = this.props;
+    const { entry, widgetFor, getAsset } = this.props;
     let image = getAsset(entry.getIn(["data", "image"]));
 
-    return <div className="mw6 center ph3 pv4">
-      <h1 className="f2 lh-title b mb3">{ entry.getIn(["data", "title"])}</h1>
-      <div className="flex justify-between grey-3">
-        <p>{ format(entry.getIn(["data", "date"]), "ddd, MMM D, YYYY") }</p>
-        <p>Read in x minutes</p>
-      </div>
-      <div className="cms mw6">
-        <p>{ entry.getIn(["data", "description"]) }</p>
-        { image && <img src={ image } alt={ entry.getIn(["data", "title"])} /> }
-        { widgetFor("body") }
+    return <div class="city-with-rupeek">
+      <h1>{entry.getIn(["data", "title"])}</h1>
+      <div class="meta-data">
+        <p>{entry.getIn(["data", "description"])}</p>
+
+        {widgetFor("body")}
+        <ul class="three-Fs">
+          <li>
+            <h4>Fair</h4>
+          Choose from our range of low-interest loans as
+          per the &lt;gold rate in Bangalore&gt; with the zero
+          hidden charges, with the help of our interest rate
+          calculator.
+        </li>
+          <li>
+            <h4>Fast</h4>
+          Get instant loan disbursal within 30 minutes
+        </li>
+          <li>
+            <h4>Flexible</h4>
+          Repay the loan amount in monthly installments
+          or within a single shot
+        </li>
+        </ul>
       </div>
     </div>;
+
   }
 }
+
+
+
+
+
+
+
+
+
+
+{/* <section class="city-with-rupeek">
+  <h1>{ entry.getIn(["data", "title"])}</h1>
+  <div class="meta-data">
+    <p>{ entry.getIn(["data", "description"]) }</p>
+    <br>
+    { widgetFor("body") }
+    <ul class="three-Fs">
+      <li>
+        <h4>Fair</h4>
+        Choose from our range of low-interest loans as
+        per the &lt;gold rate in Bangalore&gt; with the zero
+        hidden charges, with the help of our interest rate
+        calculator.
+      </li>
+      <li>
+        <h4>Fast</h4>
+        Get instant loan disbursal within 30 minutes
+      </li>
+      <li>
+        <h4>Flexible</h4>
+        Repay the loan amount in monthly installments
+        or within a single shot
+      </li>
+    </ul>
+  </div>
+</section> */}
